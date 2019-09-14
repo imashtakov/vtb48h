069d110d609af00af29fbb3d714d4cb54e6ec40e
Vladislav Cheshenko <epotajni1488@gmail.com>
2019-09-14 15:10:07 +0300

    retrofit init

--- app/src/main/java/com/example/vtbhackathonproject/App.kt
+++ app/src/main/java/com/example/vtbhackathonproject/App.kt
@@ -1,10 +1,22 @@
 package com.example.vtbhackathonproject
 
 import android.app.Application
+import com.google.gson.Gson
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class App : Application() {
 
+    lateinit var retrofit: Retrofit
+
     override fun onCreate() {
         super.onCreate()
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create(Gson()))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
+            .build()
     }
 }
